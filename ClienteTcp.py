53a9ec7075374450eff224c89c55879607fb41df
Rickzzz <146898598+rickzz0@users.noreply.github.com>
2024-09-15 23:59:56 -0300

    Delete ClienteTcp.py

--- ClienteTcp.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("127.0.0.1", 5050))
-
-while True:
-	client.send(input("").encode())
-	dados = client.recv(2048).decode()
-	print(dados)
